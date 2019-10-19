b64c6f4cdeaada1582c61249cf9250b96a038b6f
Frank Sandqvist <kankki@gmail.com>
2019-10-19 09:53:34 +0300

    Add metadata image

--- html_template.py
+++ html_template.py
@@ -5,6 +5,7 @@ html_template = '''
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta property="og:image" content="{photo_url}">
         <title>Artificiellt Österbotten - {headline}</title>
         <link
             href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap"
